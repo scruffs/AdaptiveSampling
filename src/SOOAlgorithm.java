69024c02cb7cdbaafdb830d02b4a36cc292d9839
scruffs <davenenglish@gmail.com>
2017-01-19 17:14:19 +0000

    Began refactoring the adaptive sampling algorithm to make it clean.

--- src/SOOAlgorithm.java
+++ src/SOOAlgorithm.java
@@ -1,41 +1,57 @@
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
-
 /**
  * Created by Dave on 07/10/2016.
  * The implementation of the Simultaneous Optimistic Optimisation algorithm developed by Remi Munos.
  * A description can be found at http://chercheurs.lille.inria.fr/~munos/papers/files/FTML2012.pdf.
  */
 public class SOOAlgorithm {
+    private final int startIterations = 0;
+    private final int endIterations = 22;
+    private Leaf maxLeaf = null;
+    private double testMax;
+    private int testNodeDepth;
 
-    public void AdapSamp() {
-
+    public void AdapaptiveSampling() {
         // This is the actual algorithm to expand nodes around the maximum yVal leaves
-        for (int i = 0; i < 22; i++) {
+        for (int i = startIterations; i < endIterations ; i++) {
             // Find the max yVal leaf at each node depth or below
-            for (int j = Node.maxDepth; j > 0; j--) {
-                Leaf maxLeaf = null;
-                double testMax= -1000.0;
-                for (Leaf in : Leaf.unexpandedLeaves) {
-                    if (in.getDepth() <= j & !in.getExpanded() & in.getyVal() > testMax) {
-                        maxLeaf = in;
-                        testMax = in.getyVal();
-                    }
-                }
-                // Check that an unexpanded leaf is available for each depth. Then expand it and write the data to file
+            for (testNodeDepth = Node.maxDepth; testNodeDepth > 0; testNodeDepth--) {
+                ResetTestValues();
+                FindMaxUnxpandedLeaf();
+
+                // Check that an unexpanded leaf is available for each depth.
+                // Then expand it and write the data to file
                 if (maxLeaf != null) {
                     System.out.println("Expanding!" + maxLeaf.getDepth());
                     maxLeaf.ExpandLeaf();
-                    j = maxLeaf.getDepth();
+                    testNodeDepth = maxLeaf.getDepth();
                 }
             }
-
         }
+    }
 
+    private void ResetTestValues() {
+        testMax = -1000.0;
+        maxLeaf = null;
+    }
 
+    private void FindMaxUnxpandedLeaf() {
+        for (Leaf in : Leaf.unexpandedLeaves) {
+            if (TestIfBiggerThanMax(in)) {
+                maxLeaf = in;
+                testMax = in.getyVal();
+            }
+        }
+    }
+
+    private boolean TestIfBiggerThanMax(Leaf in){
+        return CheckNodeDepth(in) & CheckIfMax(in);
+    }
+
+    private boolean CheckNodeDepth(Leaf in){
+        return in.getDepth() <= testNodeDepth;
+    }
+
+    private boolean CheckIfMax(Leaf in){
+        return in.getyVal() > testMax;
     }
 }
