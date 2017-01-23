9a7fb659670211063b5a41b431f780fcdbd159a2
scruffs <davenenglish@gmail.com>
2017-01-23 15:08:35 +0000

    Further refactoring of the adaptive sampling algorithm to make it clean.

--- src/SOOAlgorithm.java
+++ src/SOOAlgorithm.java
@@ -4,27 +4,19 @@
  * A description can be found at http://chercheurs.lille.inria.fr/~munos/papers/files/FTML2012.pdf.
  */
 public class SOOAlgorithm {
-    private final int startIterations = 0;
-    private final int endIterations = 22;
+    private final int startIterationNum = 0;
+    private final int endIterationNum = 22;
     private Leaf maxLeaf = null;
     private double testMax;
     private int testNodeDepth;
 
     public void AdapaptiveSampling() {
         // This is the actual algorithm to expand nodes around the maximum yVal leaves
-        for (int i = startIterations; i < endIterations ; i++) {
-            // Find the max yVal leaf at each node depth or below
+        for (int i = startIterationNum; i < endIterationNum; i++) {
             for (testNodeDepth = Node.maxDepth; testNodeDepth > 0; testNodeDepth--) {
                 ResetTestValues();
                 FindMaxUnxpandedLeaf();
-
-                // Check that an unexpanded leaf is available for each depth.
-                // Then expand it and write the data to file
-                if (maxLeaf != null) {
-                    System.out.println("Expanding!" + maxLeaf.getDepth());
-                    maxLeaf.ExpandLeaf();
-                    testNodeDepth = maxLeaf.getDepth();
-                }
+                ExpandMaxLeaf();
             }
         }
     }
@@ -53,5 +45,13 @@ public class SOOAlgorithm {
 
     private boolean CheckIfMax(Leaf in){
         return in.getyVal() > testMax;
+    }
+
+    private void ExpandMaxLeaf() {
+        if (maxLeaf != null) {
+            System.out.println("Expanding!" + maxLeaf.getDepth());
+            maxLeaf.ExpandLeaf();
+            testNodeDepth = maxLeaf.getDepth();
+        }
     }
 }
